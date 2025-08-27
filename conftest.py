20672427b03280d92922c8bec42521006bbb4dd2
Valentin Sharonov <valentin.sharonov@gmail.com>
2025-08-27 16:00:49 +0300

    Refactor pytest fixtures in conftest.py: parameterize book addition and genre setting

--- conftest.py
+++ conftest.py
@@ -6,38 +6,25 @@ from books_collector import BooksCollector
 def collector():
     return BooksCollector()
 
+
 @pytest.fixture
-def add_one_book(collector):
-    name = 'Гордость и предубеждение и зомби'
+def add_one_book(request, collector):
+    name = request.param
     collector.add_new_book(name)
     return name
 
+
 @pytest.fixture
-def add_one_book_with_genre(collector):
-    name = 'Гордость и предубеждение и зомби'
-    genre = 'Ужасы'
+def add_one_book_with_genre(request, collector):
+    name, genre = request.param
     collector.add_new_book(name)
     collector.set_book_genre(name, genre)
     return name, genre
 
+
 @pytest.fixture
-def add_multiple_books(collector):
-    books = [
-        ['Гордость и предубеждение и зомби', 'Ужасы'],
-        ['Что делать, если ваш кот хочет вас убить', 'Ужасы'],
-        ['Анна Каренина против вампиров', 'Комедии'],
-        ['Тихий Дон и громкая вечеринка', 'Мультфильмы']
-    ]
-    for name, genre in books:
+def add_multiple_books(request, collector):
+    for name, genre in request.param:
         collector.add_new_book(name)
         collector.set_book_genre(name, genre)
-    return books
-
-@pytest.fixture
-def add_one_book_in_favorite(collector):
-    name = 'Гордость и предубеждение и зомби'
-    genre = 'Ужасы'
-    collector.add_new_book(name)
-    collector.set_book_genre(name, genre)
-    collector.add_book_in_favorites(name)
-    return name, genre
+    return request.param
