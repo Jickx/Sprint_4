a156a5071b158bebe2fa5203eaa1270c1f525ef7
Valentin Sharonov <valentin.sharonov@gmail.com>
2025-08-29 13:14:03 +0300

    Refactor test_books_collector.py: rule "one test = one method" Instead of helping methods using direct change in dict/lists.

--- test_books_collector.py
+++ test_books_collector.py
@@ -12,123 +12,117 @@ class TestBooksCollector:
     @pytest.mark.parametrize('name', ['A', 'A' * 15, 'A' * 40])
     def test_add_new_book_valid_length(self, name, collector):
         collector.add_new_book(name)
-        assert name in collector.get_books_genre()
+        assert name in collector.books_genre
 
     @pytest.mark.parametrize('name', ['', 'A' * 41, 'A' * 100])
     def test_add_new_book_invalid_length(self, name, collector):
         collector.add_new_book(name)
-        assert name not in collector.get_books_genre()
+        assert name not in collector.books_genre
 
     def test_add_new_book_genre_is_empty(self, collector):
         name = 'Гордость и предубеждение и зомби'
         collector.add_new_book(name)
-        assert collector.get_book_genre(name) == ''
+        assert collector.books_genre[name] == ''
 
     def test_add_new_book_duplicate_book(self, collector):
         name = 'Гордость и предубеждение и зомби'
         collector.add_new_book(name)
         collector.add_new_book(name)
-        assert name in collector.books_genre
         assert len(collector.books_genre) == 1
+        assert name in collector.books_genre
 
     def test_set_book_genre_valid_genre(self, collector):
         name = 'Гордость и предубеждение и зомби'
         genre = 'Ужасы'
-        collector.add_new_book(name)
+        collector.books_genre[name] = ''
         collector.set_book_genre(name, genre)
-        assert collector.get_book_genre(name) == genre
+        assert collector.books_genre[name] == genre
 
     def test_set_book_genre_invalid_genre(self, collector):
         name = 'Гордость и предубеждение и зомби'
         genre = 'Аниме'
-        collector.add_new_book(name)
+        collector.books_genre[name] = ''
         collector.set_book_genre(name, genre)
-        assert collector.get_book_genre(name) == ''
+        assert collector.books_genre[name] == ''
 
     def test_get_book_genre_gets_genre_by_name(self, collector):
         name = 'Что делать, если ваш кот хочет вас убить'
         genre = 'Ужасы'
-        collector.add_new_book(name)
-        collector.set_book_genre(name, genre)
+        collector.books_genre[name] = genre
         assert collector.get_book_genre(name) == genre
 
-    def test_get_books_with_specific_genre_includes_books_only_given_genre(self, collector):
-        books = [
-            ('Гордость и предубеждение и зомби', 'Ужасы'),
-            ('Что делать, если ваш кот хочет вас убить', 'Ужасы'),
-            ('Анна Каренина против вампиров', 'Комедии'),
-            ('Тихий Дон и громкая вечеринка', 'Мультфильмы')
-        ]
-        for name, genre in books:
-            collector.add_new_book(name)
-            collector.set_book_genre(name, genre)
+    def test_get_books_with_specific_genre_gets_books_only_given_genre(self, collector):
+        collector.books_genre = {
+            'Гордость и предубеждение и зомби': 'Ужасы',
+            'Что делать, если ваш кот хочет вас убить': 'Ужасы',
+            'Анна Каренина против вампиров': 'Комедии',
+            'Тихий Дон и громкая вечеринка': 'Мультфильмы'
+        }
         result = collector.get_books_with_specific_genre('Ужасы')
-        assert set(result) == {'Гордость и предубеждение и зомби', 'Что делать, если ваш кот хочет вас убить'}
+        assert set(result) == {
+            'Гордость и предубеждение и зомби',
+            'Что делать, если ваш кот хочет вас убить'
+        }
 
-    def test_get_books_genre_current_books_genre_dict(self, collector):
-        name = 'Гордость и предубеждение и зомби'
-        collector.add_new_book(name)
-        assert collector.get_books_genre() == {'Гордость и предубеждение и зомби': ''}
+    def test_get_books_genre_gets_current_books_genre_dict(self, collector):
+        collector.books_genre = {
+            'Гордость и предубеждение и зомби': ''
+        }
+        assert collector.get_books_genre() == {
+            'Гордость и предубеждение и зомби': ''
+        }
 
     def test_get_books_for_children_gets_books_without_age_rating(self, collector):
-        books = [
-            ('Гордость и предубеждение и зомби', 'Ужасы'),
-            ('Что делать, если ваш кот хочет вас убить', 'Ужасы'),
-            ('Анна Каренина против вампиров', 'Комедии'),
-            ('Тихий Дон и громкая вечеринка', 'Мультфильмы')
-        ]
-        for name, genre in books:
-            collector.add_new_book(name)
-            collector.set_book_genre(name, genre)
+        collector.books_genre = {
+            'Гордость и предубеждение и зомби': 'Ужасы',
+            'Что делать, если ваш кот хочет вас убить': 'Детективы',
+            'Анна Каренина против вампиров': 'Комедии',
+            'Тихий Дон и громкая вечеринка': 'Мультфильмы'
+        }
         assert set(collector.get_books_for_children()) == {
             'Анна Каренина против вампиров',
             'Тихий Дон и громкая вечеринка'
         }
 
-    def test_get_books_for_children_in_children_list_there_no_adult_books(self, collector):
-        books = [
-            ('Гордость и предубеждение и зомби', 'Ужасы'),
-            ('Что делать, если ваш кот хочет вас убить', 'Ужасы'),
-            ('Анна Каренина против вампиров', 'Комедии'),
-            ('Тихий Дон и громкая вечеринка', 'Мультфильмы')
+    def test_get_books_for_children_in_children_list_no_adult_books(self, collector):
+        collector.books_genre = {
+            'Гордость и предубеждение и зомби': 'Ужасы',
+            'Что делать, если ваш кот хочет вас убить': 'Детективы',
+            'Анна Каренина против вампиров': 'Комедии',
+            'Тихий Дон и громкая вечеринка': 'Мультфильмы'
+        }
+        adult_books = [
+            'Гордость и предубеждение и зомби',
+            'Что делать, если ваш кот хочет вас убить'
         ]
-        for name, genre in books:
-            collector.add_new_book(name)
-            collector.set_book_genre(name, genre)
-        children_books = collector.get_books_for_children()
-        for name, genre in books:
-            if genre in collector.genre_age_rating:
-                assert name not in children_books
+        for adult_book in adult_books:
+            assert adult_book not in collector.get_books_for_children()
 
     def test_add_book_in_favorites_adds_book_in_favorites(self, collector):
         name = 'Гордость и предубеждение и зомби'
-        collector.add_new_book(name)
+        collector.books_genre[name] = ''
         collector.add_book_in_favorites(name)
-        assert name in collector.get_list_of_favorites_books()
+        assert name in collector.favorites
 
     def test_add_book_in_favorites_same_book_not_duplicate(self, collector):
         name = 'Гордость и предубеждение и зомби'
-        collector.add_new_book(name)
+        collector.books_genre[name] = ''
         collector.add_book_in_favorites(name)
         collector.add_book_in_favorites(name)
-        assert len(collector.get_list_of_favorites_books()) == 1
-        assert name in collector.get_list_of_favorites_books()
+        assert len(collector.favorites) == 1
+        assert name in collector.favorites
 
     def test_delete_book_from_favorites_deletes_book(self, collector):
         name = 'Гордость и предубеждение и зомби'
-        collector.add_new_book(name)
-        collector.add_book_in_favorites(name)
+        collector.favorites.append(name)
         collector.delete_book_from_favorites(name)
-        assert name not in collector.get_list_of_favorites_books()
+        assert name not in collector.favorites
 
     def test_get_list_of_favorites_books_returns_all(self, collector):
-        books = [
+        collector.favorites = [
             'Гордость и предубеждение и зомби',
             'Что делать, если ваш кот хочет вас убить',
             'Анна Каренина против вампиров',
             'Тихий Дон и громкая вечеринка'
         ]
-        for name in books:
-            collector.add_new_book(name)
-            collector.add_book_in_favorites(name)
-        assert set(collector.get_list_of_favorites_books()) == set(books)
+        assert set(collector.get_list_of_favorites_books()) == set(collector.favorites)
