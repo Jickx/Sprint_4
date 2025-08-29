6d70a696fccfe21c5fbf0f9eae970211df5b2664
Valentin Sharonov <valentin.sharonov@gmail.com>
2025-08-29 11:35:53 +0300

    Update conftest.py: remove unnecessary fixtures

--- conftest.py
+++ conftest.py
@@ -5,26 +5,3 @@ from books_collector import BooksCollector
 @pytest.fixture
 def collector():
     return BooksCollector()
-
-
-@pytest.fixture
-def add_one_book(request, collector):
-    name = request.param
-    collector.add_new_book(name)
-    return name
-
-
-@pytest.fixture
-def add_one_book_with_genre(request, collector):
-    name, genre = request.param
-    collector.add_new_book(name)
-    collector.set_book_genre(name, genre)
-    return name, genre
-
-
-@pytest.fixture
-def add_multiple_books(request, collector):
-    for name, genre in request.param:
-        collector.add_new_book(name)
-        collector.set_book_genre(name, genre)
-    return request.param
